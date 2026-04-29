942cda53ff88a11d4acef7fcb6fd572e9f57be6a
adiiaa <adia.uwase24snhu@keplercollege.ac.rw>
2026-04-29 13:16:33 +0200

    updating router class

--- src/test/java/base/SpecBuilder.java
+++ src/test/java/base/SpecBuilder.java
@@ -7,13 +7,13 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
-import static group4.TokenManager.getAccessToken;
+//import static group4.TokenManager.getAccessToken;
 import static routes.Routes.BASE_URL;
 
 
 public class SpecBuilder {
 
-    static String access_token = getAccessToken();
+   // static String access_token = getAccessToken();
 
     public static RequestSpecification getRequestSpec(){
         RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
@@ -23,14 +23,14 @@ public class SpecBuilder {
         return requestSpecBuilder.build();
     }
 
-    public static RequestSpecification getTokenRequestSpec(){
-        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
-        requestSpecBuilder.setBaseUri(BASE_URL);
-        requestSpecBuilder.setContentType(ContentType.JSON);
-        requestSpecBuilder.addHeader("Authorization", "Bearer" + access_token);
-        requestSpecBuilder.log(LogDetail.ALL);
-        return requestSpecBuilder.build();
-    }
+//    public static RequestSpecification getTokenRequestSpec(){
+//        RequestSpecBuilder requestSpecBuilder = new RequestSpecBuilder();
+//        requestSpecBuilder.setBaseUri(BASE_URL);
+//        requestSpecBuilder.setContentType(ContentType.JSON);
+//        requestSpecBuilder.addHeader("Authorization", "Bearer" + access_token);
+//        requestSpecBuilder.log(LogDetail.ALL);
+//        return requestSpecBuilder.build();
+//    }
 
     public static ResponseSpecification getResponseSpec(){
         ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
--- src/test/java/routes/Routes.java
+++ src/test/java/routes/Routes.java
@@ -2,8 +2,11 @@ package routes;
 
 public class Routes {
     public static final String BASE_URL = "https://dummyjson.com";
-    public static final String USERS = "/users";
-    public static final String POST = "/add";
+    public static final String GET_ALL_USERS = "/users";
+    public static final String GET_SINGLE_USER = "/users/{id}";
+    public static final String CREATE_USER = "/users/add";
+    public static final String UPDATE_USER = "/users/{id}";
+    public static final String DELETE_USER = "/users/{id}";
     public static final String AUTH = "/auth";
     public static final String LOGIN = "/login";
 }
